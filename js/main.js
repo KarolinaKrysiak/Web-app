2f6251a10607ce959119f8f8dcc8cb803a7fed14
Mika-Lorenzen <73820000+Mika-Lorenzen@users.noreply.github.com>
2021-10-05 19:38:56 +0200

    Same again

--- js/main.js
+++ js/main.js
@@ -47,3 +47,64 @@ function createHabit() {
   _habitRef.add(newHabit);
   navigateTo("tasks");
 }
+
+/* global variables: _user _selectedUserId*/
+let _user = [];
+let _selectedUserId;
+
+/*
+Fetches json data from the file user.json
+*/
+async function getUser() {
+  let response = await fetch("json/user.json");
+  let data = await response.json();
+  console.log(data);
+  _user = data;
+  appendUser(data);
+}
+
+getUser();
+
+function appendUser(users) {
+  let htmlTemplate = "";
+  for (let user of users) {
+    htmlTemplate += /*html*/ `
+    <button onclick="goToEdit(${user.id})" id="edit-button">Edit info</button>
+      <article class="${user.status}">
+        <article>
+          <img src="${user.img}">
+          <h2>Name: ${user.name}</h2>
+          <h3> Email: ${user.email}</h3>
+        </article>
+        
+      </article>
+    `;
+  }
+  document.querySelector("#user-container").innerHTML = htmlTemplate;
+}
+
+function goToEdit(id) {
+  // save id in global variable
+  _selectedUserId = id;
+  // find user to edit by using array.find and id
+  const userToEdit = _user.find((user) => user.id === _selectedUserId);
+  // set input field values with the userToEdit properties
+  document.querySelector("#nameEdit").value = userToEdit.name;
+  document.querySelector("#emailEdit").value = userToEdit.email;
+  document.querySelector("#imgEdit").value = userToEdit.img;
+  //navigate to edit view
+  navigateTo("editprofile");
+}
+
+function saveUser() {
+  // find index of the user to update in _user
+  let index = _user.findIndex((user) => user.id === _selectedUserId);
+  // update values of user in array
+  _user[index].name = document.querySelector("#nameEdit").value;
+  _user[index].email = document.querySelector("#emailEdit").value;
+  _user[index].img = document.querySelector("#imgEdit").value;
+  // update dom usind appendUser()
+  appendUser(_user);
+  //navigating back
+  navigateTo("products");
+}
